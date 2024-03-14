409503feab355b764bd7fabc69fe6d2a0e070f43
Nabin1234 <nabinacharofficial@gmail.com>
2024-03-14 13:56:18 +0530

    fix issues

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,25 @@
 import CustomInput from "@/components/CustomInput";
-import { Box, Button, Container, Typography, styled } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  Typography,
+  styled,
+} from "@mui/material";
 import dynamic from "next/dynamic";
-import React, { useCallback, useEffect, useState } from "react";
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import { useForm } from "react-hook-form";
-import { useClipboard } from '@mantine/hooks';
-import _, { map } from 'underscore';
-import ReactQuill from "react-quill";
+import { useClipboard } from "@mantine/hooks";
+import _, { map } from "underscore";
+import ReactQuill, { Quill } from "react-quill";
+
 
 const StyledContainer = styled(Container)`
   margin: 10px;
@@ -14,7 +28,7 @@ const StyledContainer = styled(Container)`
 
 const StyledBox = styled(Box)`
   margin: auto;
-  width: 50%;
+  width: 100%;
   box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.4);
   padding: 20px;
   border-radius: 10px;
@@ -36,18 +50,23 @@ const StyledNormalText = styled(Typography)`
   font-weight: bold;
 `;
 
-const QuillNoSSRWrapper = dynamic(import('react-quill'), {
-  ssr: false,
-  loading: () => <p>Loading ...</p>,
-});
+const Editor = dynamic(
+  import("@/components/Editor"),
+  {
+    ssr: false,
+    loading: () => <p>Loading ...</p>,
+  }
+);
 
 const Index = () => {
   // Get Data From User
   const { register, handleSubmit } = useForm();
-  const [ticketData, setTicketData] = useState<any>({});
-  const number = ticketData?.ticketNumber?.split(",");
-  const uniqeNumbers = _.uniq(number)
-  console.log("data", uniqeNumbers);
+
+  // Editor Quill
+  const [editorValue, setEditorValue] =
+    useState<ReactQuill.Value>("");
+
+
 
   // Date
   const date = new Date();
@@ -55,36 +74,78 @@ const Index = () => {
   let month = date.getMonth() + 1;
   let year = date.getFullYear();
   let CURRENT_DATE = `${day}/${month}/${year}`;
-  
+
   // Clipboard Hook
   const { copy, copied } = useClipboard();
-  const allTicketNumbers = `
-    Update ${CURRENT_DATE}
-    Worked on these tickets
-    ${uniqeNumbers?.map((item: any) => `${ticketData.link}/${ticketData.ticketFormat}-${item}`).join('\n')}
-  `
+  const ref=useRef<ReactQuill|null>(null)
+
   const handleCopyAll = () => {
-    copy(allTicketNumbers);
+    
+     copy(ref?.current?.getEditor().getText());
   };
 
-  // Editor Quill 
-  const [editorValue, setEditorValue] = useState('')
-  useEffect(()=>{
-    setEditorValue(allTicketNumbers)
-  },[uniqeNumbers,])
-  
+
+
+  const onSubmit = (data: any) => {
+
+    
+
+    const number =
+    data?.ticketNumber?.split(",");
+
+    const unique= _.uniq(number);
+
+
+    const allTicketNumbers = `
+    Update ${CURRENT_DATE}
+    Worked on these tickets
+    ${unique
+      ?.map(
+        (item: any) =>
+          `${data.link}/${data.ticketFormat}-${item}`
+      )
+      .join("\n")}
+  `;
+
+    setEditorValue(allTicketNumbers);
+  };
+
+ 
 
   return (
     <StyledContainer>
       <StyledBox>
-        <form onSubmit={handleSubmit((data) => setTicketData(data))}>
-          <StyledTypography>Ticket Generator</StyledTypography>
-          <StyledSecondaryText>Enter Default Link</StyledSecondaryText>
-          <CustomInput fullWidth type="text" defaultValue="https://signaturepharmacy.atlassian.net/browse" {...register("link")} />
-          <StyledSecondaryText>Enter Ticket Format</StyledSecondaryText>
-          <CustomInput fullWidth type="text" defaultValue="SCN" {...register("ticketFormat")} />
-          <StyledSecondaryText>Enter Ticket Number</StyledSecondaryText>
-          <CustomInput fullWidth type="text" {...register("ticketNumber")} required/>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <StyledTypography>
+            Ticket Generator
+          </StyledTypography>
+          <StyledSecondaryText>
+            Enter Default Link
+          </StyledSecondaryText>
+          <CustomInput
+            fullWidth
+            type="text"
+            defaultValue="https://signaturepharmacy.atlassian.net/browse"
+            {...register("link")}
+          />
+          <StyledSecondaryText>
+            Enter Ticket Format
+          </StyledSecondaryText>
+          <CustomInput
+            fullWidth
+            type="text"
+            defaultValue="SCN"
+            {...register("ticketFormat")}
+          />
+          <StyledSecondaryText>
+            Enter Ticket Number
+          </StyledSecondaryText>
+          <CustomInput
+            fullWidth
+            type="text"
+            {...register("ticketNumber")}
+            required
+          />
           <Button
             variant="contained"
             color="success"
@@ -95,25 +156,43 @@ const Index = () => {
           </Button>
         </form>
       </StyledBox>
-      <StyledBox sx={{ mt: 5 }}>
-        <StyledTypography>Generated Ticket</StyledTypography>
-        {number ? (
-          <>
-            <StyledNormalText>Update {CURRENT_DATE}</StyledNormalText>
-            <StyledNormalText>Worked on these tickets</StyledNormalText>
-          </>
-        ) : null}
-        {uniqeNumbers?.map((item: any) => (
-          <div key={item}>
-            <StyledSecondaryText>{`${ticketData.link}/${ticketData.ticketFormat}-${item}`}</StyledSecondaryText>
-          </div>
-        ))}
-        <div dangerouslySetInnerHTML={{__html: editorValue}}/>
-        <Button variant="contained" color="info" sx={{mt: 2}} onClick={handleCopyAll}>
-        {copied ? <span>Copied!</span> : <span>Copy</span>}
-        </Button>
-        <QuillNoSSRWrapper theme="snow" value={editorValue} onChange={setEditorValue}/>
-      </StyledBox>
+      <Box sx={{ mt: 5 }}>
+        <Grid container spacing={2}>
+          <Grid item md={6}>
+            <Editor
+              theme="snow"
+              value={editorValue}
+              onInit={ref}
+              onChange={setEditorValue}
+            />
+          </Grid>
+          <Grid item md={6}>
+            <StyledBox>
+              <StyledTypography>
+                Generated Ticket
+              </StyledTypography>
+
+              <Button
+                variant="contained"
+                color="info"
+                sx={{ mt: 2 }}
+                onClick={handleCopyAll}
+              >
+                {copied ? (
+                  <span>Copied!</span>
+                ) : (
+                  <span>Copy</span>
+                )}
+              </Button>
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: editorValue,
+                }}
+              />
+            </StyledBox>
+          </Grid>{" "}
+        </Grid>
+      </Box>
     </StyledContainer>
   );
 };
